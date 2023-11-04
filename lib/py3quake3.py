bd7ec8da32ab423705a404ed2068be1257eff87d
Antoine Meresse <ant.meresse@gmail.com>
2023-11-04 23:18:59 +0100

    fix: cleanup

--- lib/py3quake3.py
+++ lib/py3quake3.py
@@ -210,22 +210,3 @@ class PyQuake3(object):
                     self.players.append(Player(int(ply[0]), ply[3], int(ply[1]), int(ply[2]), ply[5]))
                 except (IndexError, ValueError):
                     continue
-
-
-if __name__ == '__main__':
-
-    QUAKE = PyQuake3(server='antoinemeresse.xyz:27960', rcon_password='Flifli')
-
-    QUAKE.update()
-
-    print("The server name of '%s' is %s, running map %s with %s player(s)." % (QUAKE.get_address(), QUAKE.values['sv_hostname'], QUAKE.values['mapname'], len(QUAKE.players)))
-
-    for gamer in QUAKE.players:
-        print("%s with %s frags and a %s ms ping" % (gamer.name, gamer.frags, gamer.ping))
-
-    QUAKE.rcon_update()
-
-    for gamer in QUAKE.players:
-        print("%s (%s) has IP address of %s" % (gamer.name, gamer.num, gamer.address))
-
-    QUAKE.rcon('bigtext "pyquake3 is great"')
